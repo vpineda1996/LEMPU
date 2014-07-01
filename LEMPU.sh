c2b6987c291a9e248ed772c2ea174eb94d3aed77
VictorP <vpineda1996@gmail.com>
2014-07-01 14:25:03 -0500

    Added fast-cgi params

--- LEMPU.sh
+++ LEMPU.sh
@@ -1,36 +1,54 @@
+#!/bin/bash
 echo 'Welcome to this quick LEMP installer'
 echo ""
 # LEMP setup and first user creation
 echo "I need to ask you a few questions before starting the setup"
 echo "You can leave the default options and just press enter if you are ok with them"
 echo ""
+# Nginx version 1.7.2
+# MySQL version 5.6.19
+# PHP version 5.5.14
 
-# Root Dir
+# Variables used in this script
+
+# @param HOME = Defualt user home directory
+# @param LAMPDIRECTORY = Install directory
+# @param NGDIRECTORY = Configuration dir for nginx
+# @param ERASENGINX = Erase previous nginx configuration files at $HOME/.config/nginx/
+# @param MYSQLDIRECTORY = Configuration dir for MySQL
+# @param MYSQLROOTPASSWORD = MySQL root Password
+# @param PHPFPMIRECTORY = Configuration dir for PHP-FPM
+
+# Setup Root Dir
 function InstallPath {
-	echo "Where do you want me to install?"
-	read -p "Folder: " -e -i $HOME/LEMP $LAMPDIRECTORY
-	if [ ! -e $LAMPDIRECTORY ]; then
-		mkdir -p $LAMPDIRECTORY
+	if [[ -v LAMPDIRECTORY ]]; then
+		echo "Where do you want me to install?"
+		read -p "Folder: " -e -i $HOME/LEMP $LAMPDIRECTORY
+		if [ ! -e $LAMPDIRECTORY ]; then
+			mkdir -p $LAMPDIRECTORY
+		fi
 	fi
 }
-# Install and build in $LEMP
+# Install and build in $LAMPDIRECTORY
 function InstallNginx {
 	echo "Installing Nginx"
 	sleep 1
 	wget -q http://nginx.org/download/nginx-1.7.2.tar.gz
-	tar xzvf nginx-1.7.2.tar.gz
+	tar xzf nginx-1.7.2.tar.gz
 	rm nginx-1.7.2.tar.gz
 	cd nginx-1.7.2
-	./configure --prefix=$LEMP
+	./configure --prefix=$LAMPDIRECTORY
 	make
 	make install
 	cd ..
 	rm -r nginx-1.7.2
 }
-# Configure Nginx in $LEMP
+# Configure Nginx in $NGDIRECTORY
 function ConfigureNginx {
-	echo "Where do you want me to configure Nginx?"
-	read -p "Folder: " -e -i $HOME/.config/nginx NGDIRECTORY
+	if [[ -v NGDIRECTORY ]]; then
+		echo "Where do you want me to configure Nginx?"
+		read -p "Folder: " -e -i $HOME/.config/nginx NGDIRECTORY
+	fi
 	# Create the home directory
 	mkdir -p $NGDIRECTORY
 	# Create files
@@ -110,62 +128,185 @@ function CheckConfigNginx {
 	fi
 }
 
-# Install MariaDB in $LEMP
-function InstallMariaDB { 
-	echo "Installing MariaDB"
+# Install MySQL in $LAMPDIRECTORY
+function InstallMySQL { 
+	echo "Installing MySQL"
 	sleep 1
-	wget "https://downloads.mariadb.org/f/mariadb-10.0.12/source/mariadb-10.0.12.tar.gz/from/http:/ftp.utexas.edu/mariadb?serve" -q -O mariadb-10.0.12.tar.gz
-	tar xzvf mariadb-10.0.12.tar.gz
-	rm mariadb-10.0.12.tar.gz
-	cd mariadb-10.0.12
-	cmake -DCMAKE_INSTALL_PREFIX:PATH=$LEMP . && make all install
+	wget "http://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.19.tar.gz" -q -O mysql-5.6.19.tar.gz
+	tar xzf mysql-5.6.19.tar.gz		
+	rm mysql-5.6.19.tar.gz
+	cd mysql-5.6.19
+	BUILD/autorun.sh
+	./configure --prefix=$LAMPDIRECTORY/mysql
+	make
+	make install
 	cd ..
-	rm -r mariadb-10.0.12
+	rm -r mysql-5.6.19
+	if [ ! -e $LAMPDIRECTORY/init.d ]; then
+		mkdir -p $LAMPDIRECTORY/init.d
+	fi
+	mv $LAMPDIRECTORY/mysql/support-files/mysql.server $LAMPDIRECTORY/init.d/mysql.server	
 }
 
-# Install php-fpm in $LEMP  --- incomplete
+# Configure MariaDB in $MYSQLDIRECTORY
+# socket @ $MYSQLDIRECTORY/socket.sock
+# checar como inicializar MySQL
+function ConfigureMySQL {
+	if [[ -v MYSQLDIRECTORY ]]; then
+		echo "Where do you want me to configure MySQL?"
+		read -p "Folder: " -e -i $HOME/.config/mysql MYSQLDIRECTORY
+	fi
+	# Create the MySQL directory
+	if [ ! -e $MYSQLDIRECTORY ]; then
+		mkdir -p $MYSQLDIRECTORY
+	fi
+	# Setup Test tables
+	$LAMPDIRECTORY/mysql/scripts/mysql_install_db --basedir=$LAMPDIRECTORY/mysql --datadir=$MYSQLDIRECTORY --user=$USER
+	sed -i "s|\# innodb_buffer_pool_size = 128M|innodb_buffer_pool_size = 128M|" $LAMPDIRECTORY/mysql/my.cnf
+ 	sed -i "s|\# basedir = .....|basedir = $LAMPDIRECTORY/mysql|" $LAMPDIRECTORY/mysql/my.cnf
+ 	sed -i "s|\# datadir = .....|datadir = $MYSQLDIRECTORY|" $LAMPDIRECTORY/mysql/my.cnf
+ 	sed -i "s|\# socket = .....|socket = $MYSQLDIRECTORY/socket.sock|" $LAMPDIRECTORY/mysql/my.cnf
+ 	# Symbolic link to $MYSQLDIRECTORY
+ 	ln -s $LAMPDIRECTORY/mysql/my.cnf $MYSQLDIRECTORY/my.cnf
+ 	$LAMPDIRECTORY/init.d/mysql.server start
+ 	echo "What passowrd do you want for MySQL root user?"
+ 	read -p "Password: " -e -i pass MYSQLROOTPASSWORD
+ 	$LAMPDIRECTORYmysql/bin/mysqladmin -u root password '$MYSQLROOTPASSWORD' --socket=$HOME/.config/mysql/socket.sock
+}
+
+# Install php-fpm in $LAMPDIRECTORY  --- incomplete
 function InstallFPM {
 	echo "Installing PHP-FPM"
 	sleep 1
+	if [[ -v PHPFPMIRECTORY ]]; then
+		echo "Where do you want me to configure PHP-FPM?"
+		read -p "Folder: " -e -i $HOME/.config/php-fpm PHPFPMIRECTORY
+	fi
+	# Create the PHP-FPM directory
+	if [ ! -e $PHPFPMIRECTORY ]; then
+		mkdir -p $PHPFPMIRECTORY
+	fi
 	wget http://us1.php.net/get/php-5.5.14.tar.gz/from/this/mirror -q -O php-5.5.14.tar.gz
-	tar xzvf php-5.5.14.tar.gz
+	tar xzf php-5.5.14.tar.gz
 	rm php-5.5.14.tar.gz
 	cd php-5.5.14
+	./configure --prefix=$LAMPDIRECTORY --disable-cgi --enable-fpm --with-pic --with-xmlrpc --enable-sockets --enable-ipv6 --enable-json --with-config-file-path=$PHPFPMIRECTORY --with-config-file-scan-dir=$PHPFPMIRECTORY  --with-mysql=$LAMPDIRECTORY/mysql --with-mysqli=$LAMPDIRECTORY/mysql/bin/mysql_config --with-gd --with-bz2 --with-zlib --enable-mbstring --enable-calendar --enable-bcmath --enable-ftp --enable-exif --enable-zip --enable-gd-native-ttf --with-mysql-sock=$MYSQLDIRECTORY/socket.conf	
+	make
+	make install
+	# Fix prefix in init.d file to start php-fpm
+	sed -i "s|php_fpm_CONF=\${prefix}/etc/php-fpm.conf|php_fpm_CONF=$PHPFPMIRECTORY/conf|" sapi/fpm/init.d.php-fpm
+ 	sed -i "s|php_fpm_PID=\${prefix}/var/run/php-fpm.pid|php_fpm_PID=$PHPFPMIRECTORY/php-fpm.pid|" sapi/fpm/init.d.php-fpm
+ 	if [ ! -e $LAMPDIRECTORY/init.d ]; then
+		mkdir -p $LAMPDIRECTORY/init.d
+	fi
+ 	mv sapi/fpm/init.d.php-fpm $LAMPDIRECTORY/init.d/php-fpm
+ 	cd ..
+}
+function ConfigureFPM {
+	if [[ -v PHPFPMIRECTORY ]]; then
+		echo "Where do you want me to configure PHP-FPM?"
+		read -p "Folder: " -e -i $HOME/.config/php-fpm PHPFPMIRECTORY
+	fi
+	# Create the PHP-FPM directory
+	if [ ! -e $PHPFPMIRECTORY ]; then
+		mkdir -p $PHPFPMIRECTORY
+	fi
 
 }
+function StartMySQL {
+	$LAMPDIRECTORY/mysql/support-files/mysql.server start
+}
 
-
+function StartPHPFPM {
+	$LAMPDIRECTORY/init.d/php-fpm start
+}
 
 while :
 do
 	echo "What do you want to do?"
 	echo ""
-	echo "1) Setup LEMP"
-	echo "2) Install and Configure Nginx"
-	echo "3) Install and Configure MariaDB"
+	echo "1) Setup..."
+	echo "2) Configure..."
+	echo "3) Run..."
 	echo "4) Exit"
 	echo ""
 	read -p "Select an option [1-4]: " option
 	case $option in
-		1) # Setup LEMP
+		1) # Setup Menu
+			do
+				echo "1) Install and configure all"
+				echo "2) Install and Configure Nginx"
+				echo "3) Install and Configure MySQL"
+				echo "4) Install and Configure PHP-FPM"
+				echo "5) Exit"
+				echo ""
+				read -p "Select an option [1-5]: " setupOption
+				case $setupOption in
+					1) # Install and configure all
+						InstallPath
+						# Install Nginx...
+						InstallNginx
+						CheckConfigNginx
+						exit
+						;;
+					2) # Install and Configure Nginx
+						InstallPath
+						# Install Nginx...
+						InstallNginx
+						echo "Confgiuring Nginx"
+						CheckConfigNginx
+						exit
+						;;
+					3) # Install and Configure MySQL
+						InstallPath
+						InstallMySQL
+						echo "Confgiuring MySQL"
+						ConfigureMySQL
+						exit
+						;;
+					5) exit;;
+				esac
+			done
+			exit
+			;;
+		2) # Configure menu
+			do
+				echo "1) Install and Configure Nginx"
+				echo "2) Install and Configure MySQL"
+				echo "3) Install and Configure PHP-FPM"
+				echo "4) Exit"
+				echo ""
+				read -p "Select an option [1-4]: " setupOption
+				case $setupOption in
+					1) # Configure Nginx
+						echo "Confgiuring Nginx"
+						CheckConfigNginx
+						exit
+						;;
+					2) # Configure MySQL
+						if [[ -v LAMPDIRECTORY ]]; then
+							echo "Where is MySQL installed?"
+							read -p "Folder: " -e -i $HOME/LEMP $LAMPDIRECTORY
+						fi
+						ConfigureMySQL
+						exit
+						;;
+					3) # Configure PHP --missing
+						
+						exit
+						;;
+					4) exit;;
+				esac
+			done
+			exit
+			;;
+		3) # Run Menu
 			InstallPath
-			# Install Nginx...
-			InstallNginx
-			CheckConfigNginx
+			InstallMySQL
+			ConfigureMySQL
 			exit
 			;;
-		2) # Install and Configure Nginx
-			InstallPath
-			# Install Nginx...
-			InstallNginx
-			CheckConfigNginx
-			exit
-			;;
-		3)
-			InstallMariaDB
-			exit
-			;;
-		10) exit;;
+		4) exit;;
 	esac
 done
 
