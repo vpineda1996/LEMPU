71d0daaf5886ab7dc058a4926aef84c98de5b4e5
VictorP <vpineda1996@gmail.com>
2014-07-03 09:29:27 -0500

    change phpmyadmin download

--- LEMPU.sh
+++ LEMPU.sh
@@ -502,7 +502,7 @@ do
 				2) #Install phpMyAdmin
 					CheckVars
 					cd $NGROOTDIR
-					wget -q "http://downloads.sourceforge.net/project/phpmyadmin/phpMyAdmin/4.2.5/phpMyAdmin-4.2.5-all-languages.tar.gz?r=http%3A%2F%2Fwww.phpmyadmin.net%2Fhome_page%2Fdownloads.php&ts=1404334987&use_mirror=iweb" -O phpMyAdmin-4.2.5-all-languages.tar.gz
+					wget -q "http://sourceforge.net/projects/phpmyadmin/files/phpMyAdmin/4.2.5/phpMyAdmin-4.2.5-all-languages.tar.bz2/download" -O phpMyAdmin-4.2.5-all-languages.tar.gz
 					tar xzf phpMyAdmin-4.2.5-all-languages.tar.gz
 					rm phpMyAdmin-4.2.5-all-languages.tar.gz
 					mv phpMyAdmin-4.2.5-all-languages phpmyadmin
